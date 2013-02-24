3e0bd19651ec7d4083cd4684750e15777631a079
Dave Kaplan <workalicious@gmail.com>
2013-02-24 08:06:47 -0600

    force instead of merging, did a hard reset back to completed User Story #1 and did a force.

--- test/unit/flight_test.rb
+++ test/unit/flight_test.rb
@@ -4,4 +4,13 @@ class FlightTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "A flight from ORD to JFK takes 92 minutes" do
+    f = Flight.new
+    f.departure_airport = "ORD"
+    f.arrival_airport = "JFK"
+    f.miles = 740
+    assert_equal 92, f.duration
+  end
+
 end
